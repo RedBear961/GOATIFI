d45afac5cf4728f4d303d5cc421236be57bf0c77
Georgy Cheremnykh <g.cheremnykh@iva.ru>
2022-11-04 23:12:05 +0700

    Initial Commit

---
+++ LatifiCounter/LatifiCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LatifiCounterApp.swift
+//  LatifiCounter
+//
+//  Created by Георгий Черемных on 04.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct LatifiCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
